b6e09abcb19d36f509c6077632ae9882a64c27dd
Vivek Lokolakar <168566636+vivek8085@users.noreply.github.com>
2026-02-17 10:43:06 +0530

    up

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -34,11 +34,14 @@ const Login = ({ setUser }) => {
   };
 
   return (
-    alert("use credentials for Admin:admin@gmail.com, Manager:manager@gmail.com ,Employee: employee1@gmail.com , password:123456(for all)");
+    <div classname="text-red">use credentials for Admin:admin@gmail.com, Manager:manager@gmail.com ,Employee: employee1@gmail.com , password:123456(for all)</div>
     <div className="flex items-center justify-center h-screen bg-base-200">
-      {/* <div classname="text-red">use credentials for Admin:admin@gmail.com, Manager:manager@gmail.com ,Employee: employee1@gmail.com , password:123456(for all)</div> */}
       <div className="card w-96 bg-base-100 shadow-xl p-6">
   <h2 className="text-xl font-bold mb-4 text-center"><LockIcon className="w-5 h-5 mr-2 inline"/>Login</h2>
+        <marquee className="text-red-500 text-sm font-semibold mb-3">
+  Use credentials → Admin: admin@gmail.com | Manager: manager@gmail.com | Employee: employee1@gmail.com | Password: 123456 (for all)
+</marquee>
+
         <form onSubmit={handleLogin}>
           <input
             type="email"
