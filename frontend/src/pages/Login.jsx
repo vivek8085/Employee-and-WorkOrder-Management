b299150ed69cd100259831f9fe108adaed9f4c33
Vivek Lokolakar <168566636+vivek8085@users.noreply.github.com>
2026-02-17 10:31:33 +0530

    new

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -35,6 +35,7 @@ const Login = ({ setUser }) => {
 
   return (
     <div className="flex items-center justify-center h-screen bg-base-200">
+      <div classname="text-red">use credentials for Admin:admin@gmail.com, Manager:manager@gmail.com ,Employee: employee1@gmail.com , password:123456(for all)</div>
       <div className="card w-96 bg-base-100 shadow-xl p-6">
   <h2 className="text-xl font-bold mb-4 text-center"><LockIcon className="w-5 h-5 mr-2 inline"/>Login</h2>
         <form onSubmit={handleLogin}>
